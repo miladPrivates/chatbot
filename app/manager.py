6476f0bcdaa8598cc7aabba19601993024269902
milad <mil@af.f>
2023-11-11 04:40:50 +0330

    added management of app startup and shutdown

--- app/manager.py
+++ app/manager.py
@@ -1,5 +1,20 @@
+import logging
 from fastapi import FastAPI
 from controllers.routes import router
+import utils
 
 app = FastAPI()
 app.include_router(router)
+
+logger = logging.getLogger(__name__)
+
+
+@app.on_event("startup")
+def startup_event():
+    utils.setup_logging()
+    logger.info(f"app started")
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    logger.info(f"app shutting down")
